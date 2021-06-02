6b01c351c6b30849c43959bb706bbf0ad800e6c9
shirogaku <shirogaku@gmail.com>
2021-06-02 22:40:21 +0900

    Move token ABI

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { Contract, Wallet, providers }  from 'ethers';
-import * as ABI from './token-abi.js';
-import * as PancakeFactory from './contract/pancake/factory.js';
+import * as BEP20 from './contract/BEP20/bep20.js';
+import * as PancakeFactory from './contract/pancake-v2/factory.js';
 
 //Put wallet mnemonic here
 const strWalletMnemonic = '';
@@ -14,18 +14,24 @@ const walletInstance = walletMnemonic.connect(bscProvider);
 
 const contractPancakeFactory = new Contract(
   PancakeFactory.address,
-  PancakeFactory.eventPairCreated.ABI,
+  PancakeFactory.ABI,
   walletInstance
 );
 
 console.log('Pancake Factory Subscribed!');
 
-contractPancakeFactory.on(PancakeFactory.eventPairCreated.eventName, async (token0Addr, token1Addr, pairAddr) => {
+//PancakeSwap Factory PairCreated event
+contractPancakeFactory.on('PairCreated', async (token0Addr, token1Addr, pairAddr) => {
 
-  let token0Contract = new Contract(token0Addr, ABI.TokenABI, bscProvider);
-  let token1Contract = new Contract(token1Addr, ABI.TokenABI, bscProvider);
-  let token0Name = await token0Contract.symbol();
-  let token1Name = await token1Contract.symbol();
+  let token0Contract = new Contract(token0Addr, BEP20.TokenABI, bscProvider);
+  let token1Contract = new Contract(token1Addr, BEP20.TokenABI, bscProvider);
+  let token0Name, token1Name;
+  try {
+    token0Name = await token0Contract.symbol();
+    token1Name = await token1Contract.symbol();
+    console.log(`${new Date().toISOString()} | Token Pair created | ${token0Name}:${token1Name} | ${token0Addr}:${token1Addr} @ ${pairAddr}`);
+  }catch(e) {
+    console.error(e);
+  }
 
-  console.log(`${new Date().toISOString()} | Token Pair created | ${token0Name}:${token1Name} | ${token0Addr}:${token1Addr} @ ${pairAddr}`);
 });
